d06b258dbbc5d0575c477eb84a676bb5afd9155c
Clyde Shaffer <clydeshaffer@gmail.com>
2025-03-16 18:28:51 -0400

    promote new game index to prod

--- games/index.php
+++ games/index.php
@@ -4,6 +4,44 @@
 <center>
     <h2>Try these GameTank games on the web emulator!</h2>
 </center>
+<div class='gallery'>
+<?php
 
-<?php postList("game_entries"); ?>
+include '../include/db.php';
+
+$db = get_db("games");
+
+$sql = 'SELECT* from games where visibility=1';
+
+$statement = $db->prepare($sql);
+try {
+    $statement->execute();
+
+    if ($statement->rowCount() > 0) {
+        while($row = $statement->fetch()) {
+
+            $title = $row['title'];
+            $id = $row['gameID'];
+            $link = "https://gametank.zone/emulator/web?game=$id";
+            $img = "https://gametankgames.nyc3.cdn.digitaloceanspaces.com/games/phpapi/$id/mainimg.png";
+
+            echo "
+			<a href='$link'>
+			<figure class='postPreviewBox'>
+      				<img src='$img'>
+      		<figcaption>
+        		$title
+     		 </figcaption>
+    		</figure>
+    		</a>
+			";
+        }
+    }
+    http_response_code(200);
+} catch (PDOException $e) {
+    http_response_code(500);
+    exit('IDK something went sideways: ' . $e->getMessage());
+}
+?>
+</div>
 <?php include $_SERVER['DOCUMENT_ROOT'].'/include/foot.php'?>
