79c6cb3d3d9d13f42a32b96c936dfc89b34c1ce2
Clyde Shaffer <clydeshaffer@gmail.com>
2025-03-16 18:02:49 -0400

    add rom replacement api

---
+++ games/api/replacerom.php
@@ -0,0 +1,34 @@
+<?php
+
+include 'upload.php';
+include '../../include/db.php';
+
+session_start();
+
+if(isset($_SESSION["user"])) {
+    try {
+        $db = get_db("games");
+        $sql = 'SELECT * from games where gameID = :id';
+        $statement = $db->prepare($sql);
+        $statement->execute([
+          ":id" => $_POST['id']
+        ]);
+    
+        if ($statement->rowCount() > 0) {
+            if($row = $statement->fetch()) {
+                if($row['author'] == $_SESSION['user']) {
+                    http_response_code(200);
+                    $romfileKey = 'games/phpapi/' . $id . '/game.gtr';
+                    header('Content-Type: application/json; charset=utf-8');
+                    echo json_encode(['status' => 'ok', 'url' => generatePresignedUrl('gametankgames', 'nyc3', $romfileKey)]);
+                }
+            }
+        }
+    } catch (PDOException $e) {
+    }
+} else {
+    http_response_code(401);
+}
+
+
+?>
